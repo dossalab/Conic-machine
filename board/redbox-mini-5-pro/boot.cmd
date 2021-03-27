e40aad4f2205bb1c9da05f8da74af80ceccd5d55
Roman Luitko <dossalab@pm.me>
2021-03-27 11:57:26 +0300

    move boot partition to rootfs

--- board/redbox-mini-5-pro/boot.cmd
+++ board/redbox-mini-5-pro/boot.cmd
@@ -1,9 +1,9 @@
 part uuid ${devtype} ${devnum}:${bootpart} uuid
 
-setenv bootargs console=tty0 earlyprintk root=/dev/mmcblk0p2 rootwait
+setenv bootargs console=tty0 earlyprintk root=PARTUUID=${uuid} rootwait
 setenv fdtfile sun50i-h5-redbox-mini-5-pro.dtb
 
-load ${devtype} ${devnum}:${bootpart} ${kernel_addr_r} Image
-load ${devtype} ${devnum}:${bootpart} ${fdt_addr_r} ${fdtfile}
+load ${devtype} ${devnum}:${bootpart} ${kernel_addr_r} /boot/Image
+load ${devtype} ${devnum}:${bootpart} ${fdt_addr_r} /boot/${fdtfile}
 
 booti ${kernel_addr_r} - ${fdt_addr_r};
